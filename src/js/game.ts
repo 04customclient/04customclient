8326eb017a0a1379e062c0b419fb97c76d030af9
- <->
2024-09-07 16:32:12 -0300

    increase logout timer to 5 min

--- src/js/game.ts
+++ src/js/game.ts
@@ -1323,7 +1323,7 @@ class Game extends Client {
             // timers when a different tab is active, or the window has been minimized.
             // afk logout has to still happen after 90s of no activity (if allowed).
             // https://developer.chrome.com/blog/timer-throttling-in-chrome-88/
-            if (Date.now() - this.idleCycles > 90_000) {
+            if (Date.now() - this.idleCycles > 300_000) {
                 // 4500 ticks * 20ms = 90000ms
                 this.idleTimeout = 250;
                 // 500 ticks * 20ms = 10000ms
